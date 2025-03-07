b6c0a5b0de9d47a93a903efd7dbe4e3267c120a9
Laqi <laqi.logistics@gmail.com>
2025-03-07 20:05:24 +0100

    removed createPDOConnection

--- src/functions/database.php
+++ src/functions/database.php
@@ -168,19 +168,6 @@ function checkRP2pExists($hash) {
     return $rP2pCheckStmt->fetch(PDO::FETCH_ASSOC);
 }
 
-function createPDOConnection() {
-    global $user;
-    try {
-        // Recreate PDO connection if it's null
-        $pdo = new PDO("mysql:host={$user['dbHost']};dbname={$user['dbName']}", $user['dbUser'], $user['dbPass']);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo json_encode(["status" => "error", "message" => "Database connection failed: " . $e->getMessage()]);
-        exit();
-    }
-}
-
 function deleteContact($data) {
     global $pdo;
     $address = $data[2];
