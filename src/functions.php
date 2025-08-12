fdc5332baffc3dbe3ae441d49d8d23a01164c9cc
laqi-logistics <laqi.logistics@gmail.com>
2025-08-12 21:55:57 +0200

    added more naming logic

--- src/functions.php
+++ src/functions.php
@@ -2,9 +2,16 @@
 # Copyright 2025
 
 // Require all files in the functions directory
-require_once 'src/functions/pdo.php';
 require_once 'src/functions/contacts.php';
+require_once 'src/functions/message.php';
+require_once 'src/functions/p2p.php';
+require_once 'src/functions/rp2p.php';
+require_once 'src/functions/synch.php';
+require_once 'src/functions/transactions.php';
+require_once 'src/functions/wallet.php';
 
+// Require database functionality
+require_once 'src/functions/database/pdo.php';
 require_once 'src/functions/database/databaseContactInteraction.php';
 require_once 'src/functions/database/databaseDebugInteraction.php';
 require_once 'src/functions/database/databaseP2pInteraction.php';
@@ -13,28 +20,19 @@ require_once 'src/functions/database/databaseSchema.php';
 require_once 'src/functions/database/databaseSetup.php';
 require_once 'src/functions/database/databaseTransactionInteraction.php';
 
+// Require schema functionality
+require_once 'src/functions/schemas/echoSchema.php';
+require_once 'src/functions/schemas/outputSchema.php';
+require_once 'src/functions/schemas/payloads/payloadContactSchema.php';
+require_once 'src/functions/schemas/payloads/payloadMessageSchema.php';
+require_once 'src/functions/schemas/payloads/payloadP2pSchema.php';
+require_once 'src/functions/schemas/payloads/payloadRp2pSchema.php';
+require_once 'src/functions/schemas/payloads/payloadTransactionSchema.php';
+require_once 'src/functions/schemas/payloads/payloadUtilSchema.php';
 
-require_once 'src/functions/echoSchema.php';
-require_once 'src/functions/message.php';
-require_once 'src/functions/outputSchema.php';
-
-require_once 'src/functions/payloads/payloadContactSchema.php';
-require_once 'src/functions/payloads/payloadMessageSchema.php';
-require_once 'src/functions/payloads/payloadP2pSchema.php';
-require_once 'src/functions/payloads/payloadRp2pSchema.php';
-require_once 'src/functions/payloads/payloadTransactionSchema.php';
-require_once 'src/functions/payloads/payloadUtilSchema.php';
-
-
-require_once 'src/functions/p2p.php';
-require_once 'src/functions/rp2p.php';
-require_once 'src/functions/synch.php';
-require_once 'src/functions/transactions.php';
-
+// Require util functionality
 require_once 'src/functions/utils/utilDebug.php';
 require_once 'src/functions/utils/utilGeneral.php';
 require_once 'src/functions/utils/utilTransport.php';
 require_once 'src/functions/utils/utilUserInteraction.php';
-
-require_once 'src/functions/validation.php';
-require_once 'src/functions/wallet.php';
+require_once 'src/functions/utils/utilValidation.php';
