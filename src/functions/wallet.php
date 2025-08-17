f609e2fc3449a32d1edcdb259bbdc7825b9fa0a4
laqi-logistics <laqi.logistics@gmail.com>
2025-08-17 18:04:57 +0200

    added comment about restart

--- src/functions/wallet.php
+++ src/functions/wallet.php
@@ -11,6 +11,7 @@ function checkWalletExists($user, $request) {
 function generateWallet($argv) {
   global $user;
   // If config (wallet) exists query user about overwriting
+  // On restart of container keeps from appending new values
   if(file_exists("//etc//eiou//config.php") && isset($user["public"])){
     echo returnUserInputRequestOverwritingWallet();
     $decision = trim(fgets(STDIN));
