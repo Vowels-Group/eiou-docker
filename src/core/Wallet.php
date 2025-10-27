5be2c03ca37862dc14ae4b6e34886fc32a0bbf18
laqi-logistics <laqi.logistics@gmail.com>
2025-10-27 20:45:04 +0100

    added changes for new config structure

--- src/core/Wallet.php
+++ src/core/Wallet.php
@@ -8,7 +8,7 @@ class Wallet{
      * @return void
      */
     public static function generateWallet(array $argv): void {
-        // Add default user values
+        // Add default user values in defaultconfig.php
         $defaultConfig = "<?php\n";
         $defaultConfig .= "\$user['defaultFee'] = 0.1;\n";  // Default transaction fee in percent
         $defaultConfig .= "\$user['defaultCurrency'] = 'USD';\n"; // Default currency
@@ -18,7 +18,7 @@ class Wallet{
         $defaultConfig .= "\$user['p2pExpiration'] = 300;\n";  // Default expiration time for Peer to Peer requests in seconds
         $defaultConfig .= "\$user['debug'] = true;\n";  // Enable debug mode
         $defaultConfig .= "\$user['maxOutput'] = 5;\n";  // Maximum lines of output for multi-line output
-        file_put_contents('/etc/eiou/config.php', $defaultConfig, FILE_APPEND | LOCK_EX);
+        file_put_contents('/etc/eiou/defaultconfig.php', $defaultConfig, LOCK_EX);
 
         // Generate a private key
         $config = array(
@@ -34,15 +34,15 @@ class Wallet{
 
         // Generate random authentication code of length 20
         $authCode = bin2hex(random_bytes(10));
-
-        // Save the keys to config.php
-        file_put_contents('/etc/eiou/config.php', "\n" . '$user["public"]="' . addslashes($publicKey) . '";' . "\n". '$user["private"]="' . addslashes($privateKey) . '";' . "\n" . '$user["authcode"]="' . addslashes($authCode) . '";' . "\n", FILE_APPEND | LOCK_EX);
+        
+        // Save the keys to userconfig.php
+        file_put_contents('/etc/eiou/userconfig.php', "<?php\n" . '$user["public"]="' . addslashes($publicKey) . '";' . "\n". '$user["private"]="' . addslashes($privateKey) . '";' . "\n" . '$user["authcode"]="' . addslashes($authCode) . '";' . "\n", FILE_APPEND | LOCK_EX);
 
         // Output Tor address
         $torAddress = trim(file_get_contents('/var/lib/tor/hidden_service/hostname'));   
 
         // Append the Tor address to the config file
-        file_put_contents('/etc/eiou/config.php', "\n" . '$user["torAddress"]="' . addslashes($torAddress) . '";' . "\n", FILE_APPEND | LOCK_EX);
+        file_put_contents('/etc/eiou/userconfig.php', "\n" . '$user["torAddress"]="' . addslashes($torAddress) . '";' . "\n", FILE_APPEND | LOCK_EX);
 
         // Check if torAddressOnly flag is set
         if (isset($argv[2]) && strtolower($argv[2]) === 'toraddressonly') {
@@ -53,9 +53,9 @@ class Wallet{
         elseif (isset($argv[2])) {
             if (filter_var($argv[2], FILTER_VALIDATE_URL)) {
                 // Save the hostname to the configuration
-                $config_content = file_get_contents('/etc/eiou/config.php');
+                $config_content = file_get_contents('/etc/eiou/userconfig.php');
                 $config_content .= "\n" . '$user["hostname"]="' . addslashes($argv[2]) . '";' . "\n";
-                file_put_contents('/etc/eiou/config.php', $config_content, LOCK_EX);
+                file_put_contents('/etc/eiou/userconfig.php', $config_content, LOCK_EX);
                 echo returnHostnameSaved($argv[2]);
             } else {
                 echo returnInvalidHostnameFormat();
