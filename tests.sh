856dec042f2e81bc2fcf0ab7c4dbcd37fe34612f
laqi-logistics <laqi.logistics@gmail.com>
2025-11-04 01:45:03 +0100

    renamed and added test structure

--- tests.sh
+++ tests.sh
@@ -2,28 +2,43 @@
 
 # Path to builds
 declare -A builds=(
-    ["http4"]="./tests/build/http4.sh"
-    ["http10"]="./tests/build/http10.sh"
-    ["http13"]="./tests/build/http13.sh"
-    ["http37"]="./tests/build/http37.sh"
+    ["http4"]="./tests/buildfiles/http4.sh"
+    ["http10"]="./tests/buildfiles/http10.sh"
+    ["http13"]="./tests/buildfiles/http13.sh"
+    ["http37"]="./tests/buildfiles/http37.sh"
 )
 
 # Explanation of build
-declare -A descriptions=(
+declare -A descriptionsBuild=(
     ["http4"]="4 line nodes"
     ["http10"]="10 line nodes"
     ["http13"]="13 cluster nodes (small)"
     ["http37"]="37 cluster nodes (big)"
 )
 
+
+# Path to tests
+declare -A tests=(
+    ["generateTest"]="./tests/testfiles/generateTest.sh"
+)
+
+# Explanation of tests
+declare -A descriptionsTests=(
+    ["generateTest"]="Test if 'generate' command works"
+   
+)
+
+
+##############################################################################
+
 # Function to ask for builds
 askforbuilds() {
     printf "Builds to choose from:\n"
     build=($(for x in ${!builds[@]}; do echo $x; done | sort))
     for buildname in ${!builds[@]}; do
-        printf "\t %-8s : ${descriptions[${buildname}]} \n" ${buildname}
+        printf "\t %-8s : ${descriptionsBuild[${buildname}]} \n" ${buildname}
     done
-    build=$(printf '%s' 'Enter build: ' >&2; read x && printf '%s' "$x")
+    build=$(printf '%s' 'Enter build to run: ' >&2; read x && printf '%s' "$x")
 }
 
 
@@ -40,3 +55,32 @@ done
 
 # Build the build
 source ${builds[${build}]}
+printf '%s\n' '-----------------------'
+
+##############################################################################
+
+# Function to ask for tests
+askfortests() {
+    printf "Tests to choose from:\n"
+    test=($(for x in ${!tests[@]}; do echo $x; done | sort))
+    for testname in ${!tests[@]}; do
+        printf "\t %-8s : ${descriptionsTests[${testname}]} \n" ${testname}
+    done
+    test=$(printf '%s' 'Enter test to run: ' >&2; read x && printf '%s' "$x")
+}
+
+# Inquire untill existing tests entered by user
+test='a'
+x=0
+while  ! [[ ${tests[${test}]} ]]; do
+    if [[ $x -gt 0 ]]; then
+        printf "\tERROR: Test does not exist\\n"
+    fi
+    askfortests
+    x=$(( $x + 1 ))
+done
+
+# Run the test
+source ${tests[${test}]}
+
+
