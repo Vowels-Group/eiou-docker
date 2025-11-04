5695fc8aca67a8204a11da8922bbba3867cebd2a
laqi-logistics <laqi.logistics@gmail.com>
2025-11-04 15:14:11 +0100

    added generateTest

--- tests/testfiles/generateTest.sh
+++ tests/testfiles/generateTest.sh
@@ -2,15 +2,30 @@
 
 # Save container Addresses in the associative array containerAddresses
 #       containerAddresses[containerName] = containerAddress (HTTP)
-echo -e "\nGenerate pubkeys and set hostnames..."
+echo -e "\nGenerating pubkeys and setting hostnames..."
+
+totaltests="${#containers[@]}"
+passed=0
+failure=0
+
 for container in "${containers[@]}"; do
     containerAddress="http://"$container
     docker exec $container eiou generate $containerAddress
-    sleep 1
-
-    containerAddresses[$container]=$(docker exec $container php -r '$json = json_decode(file_get_contents("/etc/eiou/userconfig.json"),true); if(isset($json["hostname"])){echo $json["hostname"];}')
+     
+    # Check for success/failure
+    containerAddresses[$container]=$(docker exec $container php -r '
+        $json = json_decode(file_get_contents("/etc/eiou/userconfig.json"),true);
+        if (isset($json["hostname"])) {
+            echo $json["hostname"];
+        }
+    ')
     if [[ "${containerAddresses[${container}]}" == $containerAddress ]]; then
-        printf "Generate succesfully run for %s\n" ${container}
+        printf "Generate test %s ${GREEN}PASSED${NC}\n" ${container}
+        passed=$(( $passed + 1 ))
+    else
+        printf "Generate test %s ${RED}FAILED${NC}\n" ${container}
+        failure=$(( $failure + 1 ))
     fi
 done
 
+succesrate "${totaltests}" "${passed}" "${failure}" "'generate'"
