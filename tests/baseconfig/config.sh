4c5e08da2e6e65d62e2bf27ee107276ad64f80fd
laqi-logistics <laqi.logistics@gmail.com>
2025-11-11 18:47:42 +0100

    added baseconfig

---
+++ tests/baseconfig/config.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+############################### Base config #################################
+
+RED='\033[0;31m'            # Red for failure/error
+GREEN='\033[0;32m'          # Green for success
+YELLOW='\033[1;33m'
+NC='\033[0m'                # Revert to normal color
+CHECK='\u2714'              # Checkmark
+CROSS='\u274c'              # Crossmark
+
+#############################################################################
+
+
+
+########################## General Functionality ############################
+
+function succesrate(){
+    local totaltests="$1"
+    local passed="$2"
+    local failure="$3"
+    local testname="$4"
+    if [[ $passed == $totaltests ]]; then
+        printf "${GREEN}${CHECK} PASSED all ${testname} tests!${NC}\n"
+    elif [[ $passed == 0 ]]; then
+        printf "${RED}${CROSS} FAILED all ${testname} tests!${NC}\n"
+    else
+        printf "${GREEN}PASSED ${passed}${NC} ${testname} tests out of ${totaltests}\n"
+        printf "${RED}FAILED ${failure}${NC} ${testname} tests out of ${totaltests}\n"
+    fi
+}
+
+function determineTransport(){
+    local address="$1"
+    if [[ ${address} =~ ^https?:\/\/ ]]; then
+        echo 'http'
+
+    elif [[ ${address} =~ \.onion$ ]]; then
+        echo 'tor'
+    fi
+}
+
+#############################################################################
