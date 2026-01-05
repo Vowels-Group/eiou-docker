459f7805710e90f1102bb576dc90e69419b34137
laqi-logistics <laqi.logistics@gmail.com>
2026-01-05 13:31:34 +0100

    removed unneeded migration

--- files/src/database/DatabaseSetup.php
+++ files/src/database/DatabaseSetup.php
@@ -60,7 +60,7 @@ function freshInstall(){
             try {
                 $dbConn->exec(getDebugTableSchema());
                 $dbConn->exec(getContactsTableSchema());
-                $dbConn->exec( getAddressTableSchema());
+                $dbConn->exec(getAddressTableSchema());
                 $dbConn->exec(getBalancesTableSchema());
                 $dbConn->exec(getTransactionsTableSchema());
                 $dbConn->exec(getP2pTableSchema());
@@ -125,7 +125,7 @@ function runMigrations(PDO $pdo): array {
 
     // List of migration tables to create (added after initial release)
     $migrations = [
-        'held_transactions' => 'getHeldTransactionsTableSchema',
+       
     ];
 
     foreach ($migrations as $tableName => $schemaFunction) {
